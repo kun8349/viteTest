50f3045c0c7c10a52b75120450eb7d2aeddf1673
Kevin <a039386773@gmial.com>
2023-02-20 00:09:45 +0800

    update2/20

---
+++ src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ data }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {},
+    };
+  },
+  methods: {},
+  mounted() {
+    console.log(import.meta.env.VITE_TEXT);
+    const url = import.meta.env.VITE_URL;
+    this.$http.get(`${url}`).then((res) => {
+      console.log(res);
+      this.data = res.data.results;
+    });
+  },
+};
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
